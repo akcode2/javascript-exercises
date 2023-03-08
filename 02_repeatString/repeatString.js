f8141e227215554059e47cb2f89ac26e7473610d
akcode2 <40044933+akcode2@users.noreply.github.com>
2023-03-07 19:11:16 -0600

    Add loop logic to concatenate string n times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0)
+    {
+        return 'ERROR';
+    }
+    let newString = "";
+    let i = 0;
 
+    while (i < num) {
+        newString += string;
+        i++;
+    }
+    return newString;
 };
 
 // Do not edit below this line
