42175728844000b3d76a280eee59864d597096f8
akcode2 <40044933+akcode2@users.noreply.github.com>
2023-03-07 19:15:45 -0600

    Add solution: Convert string to array to reverse characters, then convert back to string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    // Turn the string into an array
+    let strArray = string.split("");
+    strArray.reverse();
+    // Turn the array back into a string
+    let newString = strArray.join("");
+    return newString;
 };
 
 // Do not edit below this line
