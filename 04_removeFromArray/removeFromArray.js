6a2ba90acda591b11334fd3cafe13990fc181cce
akcode2 <40044933+akcode2@users.noreply.github.com>
2023-03-12 13:35:44 -0500

    Add new solution that pushes elements into a new array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,18 +1,33 @@
 const removeFromArray = function(array, ...args) {
-    let length = args.length;
-    let argToFind = args[0];
-
-    for (let i = 0; i < length; i++) {
-        let argToFind = args[i];
-        // Arrow functions have implicit return. If element
-        // strictly equals argToFind, return truthy value.
-        let finderFunction = (element) => element === argToFind;
-        let foundIndex = array.findIndex(finderFunction);
-        // Remove 1 element at the foundIndex position
-        array.splice(foundIndex, 1);
-    }
     
-    return array;
+    // My first solution
+    // let length = args.length;
+    // for (let i = 0; i < length; i++) {
+    //     let argToFind = args[i];
+    //     // Arrow functions have implicit return. If element
+    //     // strictly equals argToFind, return truthy value.
+    //     let finderFunction = (element) => element === argToFind;
+    //     let foundIndex = array.findIndex(finderFunction);
+    //     // If arg is not in the array, go to the next one
+    //     if (foundIndex === -1) {
+    //         continue;
+    //     }
+    //     // Remove 1 element at the foundIndex position
+    //     array.splice(foundIndex, 1);
+    // }
+    
+    // return array;
+    
+    // Another solution pushing elements into a new array
+    const newArray = [];
+
+    array.forEach(element => {
+        if (!args.includes(element)) {
+            newArray.push(element);
+        }
+    })
+
+    return newArray;
 };
 
 // Do not edit below this line
