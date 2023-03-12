6d84a6fa4ea21867fc2fc61b5c55f74cb1703fad
akcode2 <40044933+akcode2@users.noreply.github.com>
2023-03-12 13:01:11 -0500

    Find index of element to remove, then remove it with .splice()

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
 const removeFromArray = function(array, ...args) {
+    let length = args.length;
+    let argToFind = args[0];
 
+    for (let i = 0; i < length; i++) {
+        let argToFind = args[i];
+        // Arrow functions have implicit return. If element
+        // strictly equals argToFind, return truthy value.
+        let finderFunction = (element) => element === argToFind;
+        let foundIndex = array.findIndex(finderFunction);
+        // Remove 1 element at the foundIndex position
+        array.splice(foundIndex, 1);
+    }
+    
+    return array;
 };
 
 // Do not edit below this line
